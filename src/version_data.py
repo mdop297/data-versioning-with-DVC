302840d1ed71bbe67ef67471ddc14e555f6062ec
mdop297 <lenhatminh297@gmail.com>
2024-09-30 12:41:03 +0200

    Updated version of the data from v2 to v3

--- src/version_data.py
+++ src/version_data.py
@@ -7,10 +7,8 @@ from src.utils.data_utils import initialize_dvc, initialize_dvc_storage, make_ne
 @get_config(config_path="../configs", config_name="config")
 def version_data(config: Config) -> None:
     initialize_dvc()
-
-    # initialize_dvc_storage(config.dvc_remote_name, config.dvc_remote_url)
-
-    # make_new_data_version(config.dvc_raw_data_folder, config.dvc_remote_name)
+    initialize_dvc_storage(config.dvc_remote_name, config.dvc_remote_url)
+    make_new_data_version(config.dvc_raw_data_folder, config.dvc_remote_name)
 
 
 if __name__ == "__main__":
